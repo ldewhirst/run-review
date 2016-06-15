f6372dface717930cdaa234a95440be69b74c34e
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2016-06-14 21:23:24 -0400

    App.js edits

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var pg = require('pg');
 // var passport = require('passport');
 
 require('./models/Races');
@@ -45,7 +46,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-
+app.get('/db', function(request, response) {
+  pg. connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+        { console.error(err); response.send("Error" + err); }
+      else
+        { response.render('pages/db', {results: result.rows} );}
+    })
+  })
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
